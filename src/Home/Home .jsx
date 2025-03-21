4de8c8690920e876b29a6529163e9e1cf64b8338
Tersemajesty <okekechidera992@gmail.com>
2025-03-20 17:04:57 -0700

    profile

---
+++ src/Home/Home .jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import style from './Home.module.css'
+
+ const Home  = () => {
+    return (
+        <div className={style.home}>
+            <div className={style.container}>
+                <div className={style.imgcontainer}>
+                <img src="public/terse pics.jpg" alt="profile picture" />
+                </div>
+                <div className={style.text}>
+                <h2>OKEKE CHIDERA</h2>
+                  <p>Lagos Nigeria</p>
+                  <p >"frontend developer"</p>
+                </div>
+                <div className={style.btn}>
+                  <button onClick={() => window.open("https://github.com/Tersemajesty", "_blank")}>Github</button>
+                  <button onClick={() => window.open("https://www.linkedin.com/in/okeke-chidera1/", "_blank")}>Linkedln</button>
+                  <button onClick={() => window.open("https://x.com/Tersemajesty200", "_blank")}>Twitter</button>
+                  <button onClick={() => window.open("https://www.frontendmentor.io/profile/Tersemajesty", "_blank")}>Frontend mentor</button>
+                  <button onClick={() => window.open("https://www.instagram.com/terse3938/?hl=en", "_blank")}>instagram</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default Home
